d3f70f63ebb5f00b0ffec101bc68710f8dac9482
Viacheslav Kravchenko <vk@consulting.mk>
2021-03-08 13:03:29 +0200

    Refactoring

--- main.py
+++ main.py
@@ -15,6 +15,29 @@ def remove_emoji(text):
     return emoji.get_emoji_regexp().sub(r'', text)
 
 
+def normalize_from_field(input):
+    """
+    Removes emoji and adds new lines between words in order for the text to look better on the plot
+    """
+    return remove_emoji(input).replace(' ', '\n')
+
+
+def trim_results(input):
+    result = {}
+    j = 1
+    for i in input:
+        if j >= MEMBERS_NUMBER:
+            break
+        result[i] = input[i]
+        j = j + 1
+
+    return result
+
+
+def sort_dictionary_by_value(input, reverse=False):
+    return dict(sorted(input.items(), key=lambda item: item[1], reverse=reverse))
+
+
 # Substitute result of telegram chat json export. Tested only with text-only export, without photos/videos etc
 with open('result_test.json') as json_file:
     data = json.load(json_file)
@@ -23,7 +46,7 @@ with open('result_test.json') as json_file:
         if "from" not in message or message["from"] is None:
             continue
 
-        from_field = remove_emoji(message["from"])
+        from_field = normalize_from_field(message["from"])
         if from_field not in result_dict:
             # add a new record
             result_dict[from_field] = 1
@@ -32,37 +55,17 @@ with open('result_test.json') as json_file:
             result_dict[from_field] = result_dict[from_field] + 1
 
     # sorting records  by the number of messages
-    sorted_dict = dict(sorted(result_dict.items(), key=lambda item: item[1], reverse=True))
+    sorted_dict = sort_dictionary_by_value(result_dict, reverse=True)
 
-    sorted_trimmed_dict = {}
-    j = 1
-    for i in sorted_dict:
-        if (j >= MEMBERS_NUMBER):
-            break
-        sorted_trimmed_dict[i] = sorted_dict[i]
-        j = j + 1
+    sorted_trimmed_dict = trim_results(sorted_dict)
 
     for i in sorted_trimmed_dict:
         print(i, sorted_trimmed_dict[i])
 
-    sorted_trimmed_reversed = dict(sorted(sorted_trimmed_dict.items(), key=lambda item: item[1]))
 
-    print("---------------------------------------")
+    sorted_trimmed_reversed = sort_dictionary_by_value(sorted_trimmed_dict)
 
-    for i in sorted_trimmed_reversed:
-        print(i, sorted_trimmed_reversed[i])
-
-    with_new_lines = {}
-    for i in sorted_trimmed_reversed:
-        with_new_lines[i.replace(' ', '\n')] = sorted_trimmed_reversed[i]
-
-    print("---------------------------------------")
-
-    for i in with_new_lines:
-        print(i, with_new_lines[i])
-
-
-    plt.bar(list(with_new_lines.keys()), with_new_lines.values(), color='purple')
+    plt.bar(list(sorted_trimmed_reversed.keys()), sorted_trimmed_reversed.values(), color='purple')
     plt.tight_layout()
     plt.ylabel('Кількість повідомлень', fontsize=13)
     plt.show()
